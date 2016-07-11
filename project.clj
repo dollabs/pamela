58c1d74b854870239b6f93967ad8e57988dea738
Tom Marble <tmarble@info9.net>
2016-07-11 13:48:43 -0500

    Fixed failing cli-test usage
    Updated dependencies
    Minor fixes to the pamela and pamelad startup scripts
    
    Signed-off-by: Tom Marble <tmarble@info9.net>

--- project.clj
+++ project.clj
@@ -19,33 +19,38 @@
             :url "http://opensource.org/licenses/Apache-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  ;; logging
-                 [com.taoensso/timbre "4.5.1"]
+                 [com.taoensso/timbre "4.5.1"
+                  :exclusions [org.clojure/tools.reader]]
                  [org.slf4j/slf4j-api "1.7.21"]
                  [com.fzakaria/slf4j-timbre "0.3.2"]
                  [avenir "0.2.1"]
                  [org.clojure/tools.logging "0.3.1"
                   :exclusions [org.clojure/clojure]]
                  ;; -------
-                 [org.clojure/clojurescript "1.9.36" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.89"
+                  :scope "provided"
+                  :exclusions [org.clojure/tools.reader]]
                  [org.clojure/core.async "0.2.385"]
                  [org.clojure/tools.cli "0.3.5"]
                  [riddley "0.1.12"]
                  [environ "1.0.3"]
                  ;; required for elastich
-                 [clj-http "3.1.0"]
+                 [clj-http "3.1.0"
+                  :exclusions [commons-io]]
                  [clojurewerkz/elastisch "2.2.1" :exclusions [clj-http]]
-                 [aleph "0.4.2-alpha4"]
+                 [aleph "0.4.2-alpha6"]
                  [clj-time "0.12.0"]
                  [ring "1.5.0" :exclusions [clj-time]]
                  [ring/ring-defaults "0.2.1"]
                  [compojure "1.5.1" :exclusions [commons-codec]]
                  [enlive "1.1.6"]
                  [org.clojure/data.json "0.2.6"]
-                 [cljsjs/react-dom-server "15.2.0-0"]  ;; for sablono
-                 [cljsjs/react-dom "15.2.0-0"] ;; for sablono
-                 [org.omcljs/om "1.0.0-alpha36"]
+                 [cljsjs/react-dom-server "15.2.1-0"]  ;; for sablono
+                 [cljsjs/react-dom "15.2.1-0"] ;; for sablono
+                 [org.omcljs/om "1.0.0-alpha38"]
                  [sablono "0.7.2"]
-                 [cljs-http "0.1.41"]
+                 [cljs-http "0.1.41"
+                  :exclusions [org.clojure/core.async]]
                  ;; the following are to resolve dependency conflicts
                  [commons-codec "1.10"] ;; compojure
                  ]
