88bcc68e93b3f766d70e55b566bda4af96c50606
Tom Marble <tmarble@info9.net>
2017-02-13 16:21:53 -0600

    Updates PAMELA tests
    Re-enabled CLI tests that now leverage new gensym function
    Fixed ir-test.pamela example
    Commented out dependency on cider task in build.boot
    Updated dependencies
    
    Signed-off-by: Tom Marble <tmarble@info9.net>

--- test/cli/50_tpn.sh
+++ test/cli/50_tpn.sh
@@ -27,32 +27,7 @@ pamela -i "$CODE/test/pamela/plant.pamela" \
        -o "$RESULTS/${NUMBER}_parallel-choice.tpn.$format" \
        tpn
 
-# FUTURE: meaningingfully compare output to rubrique
-
-# remove gensym artifacts
-# NOTE: maps should be sorted by key
-
-# sed -e 's/\(:[a-z\-]*\)-[0-9][0-9]*/\1-1000/g' \
-#     "$dir/${NUMBER}_parallel-choice.tpn.$format" | sort > \
-#     "$RESULTS/${NUMBER}_EXPECTED_parallel-choice.tpn.safe.$format"
-
-# sed -e 's/\(:[a-z\-]*\)-[0-9][0-9]*/\1-1000/g' \
-#     "$RESULTS/${NUMBER}_parallel-choice.tpn.$format" | sort > \
-#     "$RESULTS/${NUMBER}_parallel-choice.tpn.safe.$format"
-
-# sed -e 's/\(:[a-z\-]*\)-[0-9][0-9]*/\1-1000/g' \
-#       "$dir/${NUMBER}_parallel-choice.tpn.$format" | \
-#       fgrep :tpn-type | cut -d: -f3 | cut -d, -f1 | sort > \
-#       "$RESULTS/${NUMBER}_EXPECTED_parallel-choice.tpn.safe.$format"
-
-# sed -e 's/\(:[a-z\-]*\)-[0-9][0-9]*/\1-1000/g' \
-#       "$RESULTS/${NUMBER}_parallel-choice.tpn.$format" | \
-#       fgrep :tpn-type | cut -d: -f3 | cut -d, -f1 | sort > \
-#       "$RESULTS/${NUMBER}_parallel-choice.tpn.safe.$format"
-
-# # if ! diff -u "$RESULTS/${NUMBER}_EXPECTED_parallel-choice.tpn.safe.$format" \
-# #      "$RESULTS/${NUMBER}_parallel-choice.tpn.safe.$format"; then
-# if ! diff -u "$dir/${NUMBER}_parallel-choice.tpn.safe.$format" \
-#      "$RESULTS/${NUMBER}_parallel-choice.tpn.safe.$format"; then
-#     exit 1
-# fi
+if ! diff -u "$dir/${NUMBER}_parallel-choice.tpn.$format" \
+     "$RESULTS/${NUMBER}_parallel-choice.tpn.$format"; then
+    exit 1
+fi
